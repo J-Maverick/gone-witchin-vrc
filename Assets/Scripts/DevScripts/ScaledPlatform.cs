60acd67da7a747d356a53a01b0a8816b0019fee8
Joey <joseph.m.white@outlook.com>
2023-05-11 11:32:44 -0500

    Made scaling platforms scale over time

--- Assets/Scripts/DevScripts/ScaledPlatform.cs
+++ Assets/Scripts/DevScripts/ScaledPlatform.cs
@@ -9,9 +9,12 @@ public class ScaledPlatform : UdonSharpBehaviour
     public PlayerStatBooster playerStatBooster;
     public PlatformPiece[] platforms;
     float currentPlayerJumpRatio = 1;
-    float yScale = 1;
-    float currentPlayerSpeedRatio = 1;
-    float xzScale = 1;
+    float yScale = 1f;
+    float yTarget = 1f;
+    float currentPlayerSpeedRatio = 1f;
+    float xzTarget = 1f;
+    float xzScale = 1f;
+    float scaleRate = 1f;
 
     private void Start()
     {
@@ -24,25 +27,34 @@ public class ScaledPlatform : UdonSharpBehaviour
     }
     private void Update()
     {
-        currentPlayerSpeedRatio = Networking.LocalPlayer.GetRunSpeed() / playerStatBooster.defaultRunSpeed;
-        currentPlayerJumpRatio = Networking.LocalPlayer.GetJumpImpulse() / playerStatBooster.defaultJumpImpulse;
 
         if (Time.frameCount % 50 == 0)
         {
-            if (yScale != currentPlayerJumpRatio || xzScale != currentPlayerSpeedRatio)
-            {
-                yScale = 1 + (0.9f * (currentPlayerJumpRatio - 1));
-                xzScale = (1 + (0.75f * (currentPlayerJumpRatio - 1))) * (1 + (0.9f * (currentPlayerSpeedRatio - 1)));
-                Vector3 scale = Vector3.one * xzScale;
-                scale.y = yScale;
-                transform.localScale = scale;
+            currentPlayerSpeedRatio = Networking.LocalPlayer.GetRunSpeed() / playerStatBooster.defaultRunSpeed;
+            currentPlayerJumpRatio = Networking.LocalPlayer.GetJumpImpulse() / playerStatBooster.defaultJumpImpulse;
 
-                Vector3 childScale = Vector3.one / Mathf.Sqrt(xzScale);
-                childScale.y = 1f / yScale;
-                foreach (PlatformPiece platform in platforms)
-                {
-                    platform.SetScale(childScale);
-                }
+            if (yTarget != currentPlayerJumpRatio || xzTarget != currentPlayerSpeedRatio)
+            {
+                yTarget = 1 + (0.9f * (currentPlayerJumpRatio - 1));
+                xzTarget = (1 + (0.75f * (currentPlayerJumpRatio - 1))) * (1 + (0.9f * (currentPlayerSpeedRatio - 1)));
+                if (yTarget == yScale) scaleRate = 2f;
+                else scaleRate = (xzTarget - xzScale) / (yTarget - yScale);
+            }
+        }
+
+        if (yScale != yTarget || xzScale != xzTarget)
+        {
+            yScale = Mathf.MoveTowards(yScale, yTarget, Time.deltaTime);
+            xzScale = Mathf.MoveTowards(xzScale, xzTarget, Time.deltaTime * scaleRate);
+            Vector3 scale = Vector3.one * xzScale;
+            scale.y = yScale;
+            transform.localScale = scale;
+
+            Vector3 childScale = Vector3.one / Mathf.Sqrt(xzScale);
+            childScale.y = 1f / yScale;
+            foreach (PlatformPiece platform in platforms)
+            {
+                platform.SetScale(childScale);
             }
         }
     }
