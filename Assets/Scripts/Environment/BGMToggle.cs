b1fb8161ef02d1c72fe6d6812641ca78b09dbf45
Superfriend <joseph.m.white@outlook.com>
2023-09-17 19:17:11 -0500

    Added BGM

---
+++ Assets/Scripts/Environment/BGMToggle.cs
@@ -0,0 +1,16 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class BGMToggle : UdonSharpBehaviour
+{
+
+    public GameObject musicObject;
+
+    public override void Interact()
+    {
+        musicObject.SetActive(!musicObject.activeSelf);
+    }
+}
