d4db51a8f67b5f34a7719fef71b4f68e22744e5c
Superfriend <joseph.m.white@outlook.com>
2023-09-19 19:43:15 -0500

    Added flashbang and blackout potions

---
+++ Assets/Scripts/Potion/Custom Potions/Flashbang.cs
@@ -0,0 +1,46 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class Flashbang : ShatterEffect
+{
+    public Animator flashAnimator;
+    public BottleCollision bottleCollision;
+    public SphereCollider potionCollider;
+
+    public float collisionEnabledTime = 0.5f;
+    public float collisionEnabledTimer = 0f;
+
+    bool effectActive = false;
+
+    private void Start()
+    {
+        potionCollider.enabled = false;
+    }
+
+    public override void OnShatter()
+    {
+        effectActive = true;
+        potionCollider.enabled = true;
+        SendCustomEventDelayedSeconds("DisableEffect", collisionEnabledTime);
+    }
+
+    public void DisableEffect() {
+        potionCollider.enabled = false;
+        effectActive = false;
+        flashAnimator.SetBool("Flash", false);
+    }
+
+    public void Flash(VRCPlayerApi player)
+    {
+        if (effectActive)
+        {
+            if (player.isLocal)
+            {
+                flashAnimator.SetBool("Flash", true);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Potion/Custom Potions/FlashbangTrigger.cs
@@ -0,0 +1,15 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class FlashbangTrigger : UdonSharpBehaviour
+{
+    public Flashbang flashbang = null;
+
+    public override void OnPlayerTriggerStay(VRCPlayerApi player)
+    {
+        if (flashbang != null) flashbang.Flash(player);
+    }
+}
